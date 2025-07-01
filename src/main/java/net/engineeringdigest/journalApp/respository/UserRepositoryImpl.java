c01961724e6ffc7fce4cabbe61a3413fda2e5870
Aman10Alam <97393767+Aman10Alam@users.noreply.github.com>
2025-07-02 01:37:24 +0530

    "Email sender and user scheduler"

--- src/main/java/net/engineeringdigest/journalApp/respository/UserRepositoryImpl.java
+++ src/main/java/net/engineeringdigest/journalApp/respository/UserRepositoryImpl.java
@@ -14,10 +14,10 @@ public class UserRepositoryImpl {
     private MongoTemplate mongoTemplate;
 
     public List<User> getUserForSA(){
-        Query query= new Query();
-        query.addCriteria(Criteria.where("userName").is("aman"));
-        List<User> users= mongoTemplate.find(query, User.class);
+        Query query = new Query();
+        query.addCriteria(Criteria.where("email").regex("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Z|a-z]{2,6}$"));
+        query.addCriteria(Criteria.where("sentimentAnalysis").is(true));
         System.out.println("Current DB: "+mongoTemplate.getDb().getName());
-        return users;
+        return mongoTemplate.find(query, User.class);
     }
 }
